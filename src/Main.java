22d69e551efa484f50f87c32d9e118a2ba7fe849
Furz1q <serbek200@mail.ru>
2024-09-29 19:22:26 +0300

    Конец

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        Author author = new Author("Лев", "Толстой");
+        Book book = new Book("Детство", 1852, author);
+        author = book.getAuthorBook();
+        Author author1 = new Author("Александр", "Пушкин");
+        Book book2 = new Book("Дубровски", 1841, author1);
+        author1 = book2.getAuthorBook();
+        book.setYearBook(1984);
+        System.out.println("book.getYearBook() = " + book.getYearBook());
+        System.out.println("Название книги - "
+                + book.getNameBook() + ", выпущена - "
+                + book.getYearBook() + ", написал книгу - "
+                + book.getAuthorBook() );
+
+
 
     }
 }
